c7e25c440ff75fb30468c5b46a637265b58e1752
FrenkiPrenki <FKordich@gmail.com>
2025-10-08 09:58:57 +0200

    Dodavanje drugih gradilišta - rev 08

--- gradiliste/urls.py
+++ gradiliste/urls.py
@@ -12,7 +12,11 @@ urlpatterns = [
     # ⬇⬇⬇ DODANO
     path("gradilista/<int:gradiliste_id>/dogadjaj/novo/", views.dogadjaj_create, name="dogadjaj_create"),
     path("gradilista/<int:gradiliste_id>/dogadjaj/<int:pk>/uredi/", views.dogadjaj_update, name="dogadjaj_update"),
-    path("gradilista/<int:gradiliste_id>/dopis/novo/", views.dopis_create_for_event, name="dopis_create_for_event"),
+    path(
+    "gradilista/<int:gradiliste_id>/dogadjaj/<int:dogadjaj_id>/dopis/novo/",
+    views.dopis_create_for_event,
+    name="dopis_create_for_event",
+),
     path("gradilista/<int:gradiliste_id>/dopis/<int:pk>/uredi/", views.dopis_update, name="dopis_update"),
 
     path("admin/", admin.site.urls),
